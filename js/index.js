8bed3a5607c6737951600c4bf9ac38eebbee4b19
Chris Buob <chris.buob.drattni@gmail.com>
2017-05-31 13:31:49 +0200

    c

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ Minimalistic Form.
 by: Matheus Marsiglio
 matmarsiglio@gmail.com
 */
+<script>
 function question1() {
+value = document.getElementById("message").value;
   if (value == 32) {
      window.alert("correct!");
   } else {
      window.alert("try again);
   }
 }
+</script>
